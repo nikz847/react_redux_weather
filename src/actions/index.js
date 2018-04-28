81b05caa1c92b5de528778b2b10f0178e5ffdca7
Nikhil Parulekar <nikhilp@qti.qualcomm.com>
2018-04-28 12:34:08 -0700

    committing final changes for weather app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_KEY = "b402f69f6d8d1c29bcc52a72744404a2";
-const ROOT_URL = `openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
---
+++ src/components/google_map.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.maps, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+  render() {
+    return(
+      <div ref="maps" />
+    );
+  }
+}
+export default GoogleMap
